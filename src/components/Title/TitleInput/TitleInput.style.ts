75589c5b3657fa73d2bb08f1b6d1162113b1bb6c
cychann <skcbdlhc129@naver.com>
2024-10-04 11:12:01 +0900

    [feat]: 상단 제목 영역 구성 컴포넌트 단위로 분리

---
+++ src/components/Title/TitleInput/TitleInput.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const TitleInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  position: absolute;
+  bottom: 50px;
+  left: 0;
+  width: 100%;
+`;
--- src/components/Title/TitleInput/TitleInput.tsx
+++ src/components/Title/TitleInput/TitleInput.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./TitleInput.style";
 import ContentEditable from "components/Common/ContentEditable/ContentEditable";
 
 export default function TitleInput() {
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+
   return (
     <S.TitleInputWrapper>
-      <ContentEditable placeholder="제목을 입력하세요" />
-      <ContentEditable placeholder="소제목을 입력하세요" />
-      {/* <S.TitleInput contentEditable />
-      <S.SubTitleInput contentEditable /> */}
+      <ContentEditable
+        placeholder="제목을 입력하세요"
+        fontSize={45}
+        onChange={setTitle}
+      />
+      <ContentEditable
+        placeholder="소제목을 입력하세요"
+        fontSize={16}
+        onChange={setSubtitle}
+      />
     </S.TitleInputWrapper>
   );
 }
