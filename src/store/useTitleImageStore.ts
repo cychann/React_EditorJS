00e82c29bbdcaa00b2a8a13877eed2475fec5993
cychann <skcbdlhc129@naver.com>
2024-10-07 12:07:44 +0900

    [feat]: titleCoverColor.ts 배경색상지정 관련 로직 추가

--- src/store/useTitleImageStore.ts
+++ src/store/useTitleImageStore.ts
@@ -2,16 +2,28 @@ import { create } from "zustand";
 
 interface StoreProps {
   titleCoverImage: string | null;
+  titleCoverColor: string | null;
   isExpanded: boolean;
 
   setTitleCoverImage: (image: string | null) => void;
+  setTitleCoverColor: (color: string | null) => void;
   setIsExpanded: (expanded: boolean) => void;
 }
 
 const useTitleImageStore = create<StoreProps>((set) => ({
   titleCoverImage: null,
+  titleCoverColor: null,
   isExpanded: false,
-  setTitleCoverImage: (image) => set({ titleCoverImage: image }),
+  setTitleCoverImage: (image) =>
+    set({
+      titleCoverImage: image,
+      titleCoverColor: null,
+    }),
+  setTitleCoverColor: (color) =>
+    set({
+      titleCoverColor: color,
+      titleCoverImage: null,
+    }),
   setIsExpanded: (expanded) => set({ isExpanded: expanded }),
 }));
 
