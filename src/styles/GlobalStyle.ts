8afa7e601a96e6b8f15daae9fdfb9f36311b3a26
cychann <skcbdlhc129@naver.com>
2024-10-08 17:11:43 +0900

    [fix]: font ttf format 수정

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -87,65 +87,65 @@ export const GlobalStyle = createGlobalStyle`
 }
 
 /* 본고딕 */
-@font-face {
+  @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-Black.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-Black.ttf') format('ttf');
     font-weight: 900;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-Bold.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-Bold.ttf') format('ttf');
     font-weight: 700;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-ExtraBold.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-ExtraBold.ttf') format('ttf');
     font-weight: 800;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-ExtraLight.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-ExtraLight.ttf') format('ttf');
     font-weight: 200;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-Light.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-Light.ttf') format('ttf');
     font-weight: 300;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-Medium.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-Medium.ttf') format('ttf');
     font-weight: 500;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-Regular.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-Regular.ttf') format('ttf');
     font-weight: 400;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-SemiBold.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-SemiBold.ttf') format('ttf');
     font-weight: 600;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Noto Sans';
-    src: url('assets/fonts/NotoSansKR-Thin.ttf') format('truetype');
+    src: url('assets/fonts/NotoSansKR-Thin.ttf') format('ttf');
     font-weight: 100;
     font-style: normal;
   }
@@ -153,100 +153,100 @@ export const GlobalStyle = createGlobalStyle`
 /* 나눔명조 */
 @font-face {
   font-family: 'Nanum Myeongjo';
-  src: url('assets/fonts/NanumMyeongjo.ttf') format('truetype');
+  src: url('assets/fonts/NanumMyeongjo.ttf') format('ttf');
   font-weight: 400;
 }
 
 @font-face {
   font-family: 'Nanum Myeongjo';
-  src: url('assets/fonts/NanumMyeongjoBold.ttf') format('truetype');
+  src: url('assets/fonts/NanumMyeongjoBold.ttf') format('ttf');
   font-weight: 700;
 }
 @font-face {
   font-family: 'Nanum Myeongjo';
-  src: url('assets/fonts/NanumMyeongjoExtraBold.ttf') format('truetype');
+  src: url('assets/fonts/NanumMyeongjoExtraBold.ttf') format('ttf');
   font-weight: 800;
 }
 
 /* 나눔고딕 */
 @font-face {
   font-family: 'Nanum Gothic';
-  src: url('assets/fonts/NanumGothic.ttf') format('truetype');
+  src: url('assets/fonts/NanumGothic.ttf') format('ttf');
   font-weight: 400;
 }
 
 @font-face {
   font-family: 'Nanum Gothic';
-  src: url('assets/fonts/NanumGothicBold.ttf') format('truetype');
+  src: url('assets/fonts/NanumGothicBold.ttf') format('ttf');
   font-weight: 700;
 }
 
 @font-face {
   font-family: 'Nanum Gothic';
-  src: url('assets/fonts/NanumGothicExtraBold.ttf') format('truetype');
+  src: url('assets/fonts/NanumGothicExtraBold.ttf') format('ttf');
   font-weight: 800;
 }
 
 @font-face {
   font-family: 'Nanum Gothic';
-  src: url('assets/fonts/NanumGothicLight.ttf') format('truetype');
+  src: url('assets/fonts/NanumGothicLight.ttf') format('ttf');
   font-weight: 300;
 }
 
 /* 나눔바른고딕 */
 @font-face {
   font-family: 'Nanum Barun Gothic';
-  src: url('assets/fonts/NanumBarunGothic.ttf') format('truetype');
+  src: url('assets/fonts/NanumBarunGothic.ttf') format('ttf');
   font-weight: 400;
 }
 
 @font-face {
   font-family: 'Nanum Barun Gothic';
-  src: url('assets/fonts/NanumBarunGothicBold.ttf') format('truetype');
+  src: url('assets/fonts/NanumBarunGothicBold.ttf') format('ttf');
   font-weight: 700;
 }
 
 @font-face {
   font-family: 'Nanum Barun Gothic';
-  src: url('assets/fonts/NanumBarunGothicLight.ttf') format('truetype');
+  src: url('assets/fonts/NanumBarunGothicLight.ttf') format('ttf');
   font-weight: 300;
 }
 
 @font-face {
   font-family: 'Nanum Barun Gothic';
-  src: url('assets/fonts/NanumBarunGothicUltraLight.ttf') format('truetype');
+  src: url('assets/fonts/NanumBarunGothicUltraLight.ttf') format('ttf');
   font-weight: 200;
 }
 
 /* Helvetica */
 @font-face {
   font-family: 'Helvetica';
-  src: url('assets/fonts/Helvetica.ttf') format('truetype');
+  src: url('assets/fonts/Helvetica.ttf') format('ttf');
   font-weight: 400;
 }
 
 @font-face {
   font-family: 'Helvetica';
-  src: url('assets/fonts/HelveticaBold.ttf') format('truetype');
+  src: url('assets/fonts/HelveticaBold.ttf') format('ttf');
   font-weight: 700;
 }
 
 @font-face {
   font-family: 'Helvetica';
-  src: url('assets/fonts/HelveticaLight.ttf') format('truetype');
+  src: url('assets/fonts/HelveticaLight.ttf') format('ttf');
   font-weight: 300;
 }
 
 /* Georgia */
 @font-face {
   font-family: 'Georgia';
-  src: url('assets/fonts/Georgia.ttf') format('truetype');
+  src: url('assets/fonts/Georgia.ttf') format('ttf');
   font-weight: 400;
 }
 
 @font-face {
   font-family: 'Georgia';
-  src: url('assets/fonts/GeorgiaBold.ttf') format('truetype');
+  src: url('assets/fonts/GeorgiaBold.ttf') format('ttf');
   font-weight: 700;
 }
 `;
