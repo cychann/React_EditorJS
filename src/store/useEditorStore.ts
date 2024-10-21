93305683e612f70ba618221c093c0313b2dddc99
cychann <skcbdlhc129@naver.com>
2024-10-21 15:03:33 +0900

    [refactor]: useEditorStore 정렬 관련 변수 및 함수 정의

--- src/store/useEditorStore.ts
+++ src/store/useEditorStore.ts
@@ -6,16 +6,19 @@ interface StoreProps {
   blokcs: EditorElement[];
   isModalOpen: boolean;
   activeModal: "place" | "emoji" | "line" | null;
+  align: "left" | "center";
 
   addBlock: (type: EditorBlockType, data?: object) => void;
   toggleModal: () => void;
   setActiveModal: (modalType: "place" | "emoji" | "line" | null) => void;
+  toggleAlign: () => void;
 }
 
 const useEditorStore = create<StoreProps>((set) => ({
   blokcs: [{ id: 0, type: "text", data: {} }],
   isModalOpen: false,
   activeModal: null,
+  align: "left",
 
   addBlock: (type, data) => {
     set((state) => ({
@@ -31,6 +34,10 @@ const useEditorStore = create<StoreProps>((set) => ({
       activeModal: !state.isModalOpen ? state.activeModal : null,
     })),
   setActiveModal: (modalType) => set({ activeModal: modalType }),
+  toggleAlign: () =>
+    set((state) => ({
+      align: state.align === "left" ? "center" : "left",
+    })),
 }));
 
 export default useEditorStore;
