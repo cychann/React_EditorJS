629f993693d2e67e680550ab4e9c7f91fd257aff
cychann <skcbdlhc129@naver.com>
2024-10-24 17:10:16 +0900

    [feat]: Unicode 기반 이모지 컴포넌트 개발

--- src/components/Editor/EditorToolModal/EmojiModal/EmojiModal.style.ts
+++ src/components/Editor/EditorToolModal/EmojiModal/EmojiModal.style.ts
@@ -3,7 +3,41 @@ import { COMMON_THEME } from "styles/Theme";
 
 export const EmojiModalWrapper = styled.div`
   width: 358px;
+  height: 400px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
+  gap: 12px;
+`;
+
+export const EmojiListWrapper = styled.ul`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(13, 22px);
+  grid-gap: 6px;
+`;
+
+export const EmojiItem = styled.li`
+  cursor: pointer;
+`;
+
+export const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const PaginationButton = styled.button`
+  margin: 0 5px;
+  padding: 8px 12px;
+  color: ${COMMON_THEME.white_primary};
+  background-color: ${COMMON_THEME.primary};
+  border: none;
+  cursor: pointer;
+
+  &:disabled {
+    background-color: ${COMMON_THEME.light_gray};
+    cursor: not-allowed;
+  }
 `;
