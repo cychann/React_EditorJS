71608b4e39ec20b174f0f4fdcc4e53f3d4d56a5a
cychann <skcbdlhc129@naver.com>
2024-11-04 18:44:33 +0900

    chore: EditorJS 관련 라이브러리 설치 및 초기 세팅

--- src/components/Editor/EditorSection/EditorSection.tsx
+++ src/components/Editor/EditorSection/EditorSection.tsx
@@ -2,9 +2,15 @@ import React, { useEffect, useRef, useState } from "react";
 import * as S from "./EditorSection.style";
 import EditorToolbar from "components/Editor/EditorToolbar/EditorToolbar";
 import EditorContent from "components/Editor/EditorContent/EditorContent";
+import EditorJS from "@editorjs/editorjs";
+import { EDITOR_JS_TOOLS } from "../EditorContent/Tools";
+import DragDrop from "editorjs-drag-drop";
+import Undo from "editorjs-undo";
 
 export default function EditorSection() {
   const editorSectionRef = useRef<HTMLDivElement>(null);
+  const editorRef = useRef<EditorJS | undefined>();
+
   const [toolbarTop, setToolbarTop] = useState(487);
 
   useEffect(() => {
@@ -28,10 +34,33 @@ export default function EditorSection() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  useEffect(() => {
+    if (!editorRef.current) {
+      const editor = new EditorJS({
+        holder: "editorjs",
+        autofocus: true,
+        tools: EDITOR_JS_TOOLS,
+        onReady: () => {
+          new Undo({ editor });
+          new DragDrop(editor);
+        },
+      });
+
+      editorRef.current = editor;
+    }
+
+    return () => {
+      if (editorRef.current && editorRef.current.destroy) {
+        editorRef.current.destroy();
+      }
+    };
+  }, []);
+
   return (
     <S.EditorSectionContainer ref={editorSectionRef}>
-      <EditorContent />
-      <EditorToolbar toolbarTop={toolbarTop} />
+      <div id="editorjs" />
+      <EditorToolbar toolbarTop={toolbarTop} editor={editorRef} />
     </S.EditorSectionContainer>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "styles/GlobalStyle";
-import "styles/Font.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
