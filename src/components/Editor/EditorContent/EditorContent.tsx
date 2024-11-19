3abdb3bfa20e9761b32cd46458470f5cf642e122
cychann <skcbdlhc129@naver.com>
2024-11-19 11:53:35 +0900

    [fix]: Editor 인스턴스 중복 생성 문제 해결
    - ref를 사용하여 Editor 인스턴스 로컬 관리
    - 전역 상태는 Editor 준비 완료 후에만 업데이트 하도록

--- src/components/Editor/EditorContent/EditorContent.tsx
+++ src/components/Editor/EditorContent/EditorContent.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import EditorJS from "@editorjs/editorjs";
 import DragDrop from "editorjs-drag-drop";
 import Undo from "editorjs-undo";
 import { EDITOR_JS_TOOLS } from "constants/editorTools";
 import useEditorStore from "store/useEditorStore";
 
-export default function EditorContent() {
-  const { editor, setEditor } = useEditorStore();
+const EditorContent = React.memo(() => {
+  const { setEditor } = useEditorStore();
+  const editorInstanceRef = useRef<EditorJS | null>(null);
 
   useEffect(() => {
-    if (!editor) {
+    if (!editorInstanceRef.current) {
       const editorInstance = new EditorJS({
         holder: "editorjs",
         autofocus: true,
@@ -17,19 +18,23 @@ export default function EditorContent() {
         onReady: () => {
           new Undo({ editor: editorInstance });
           new DragDrop(editorInstance);
+          setEditor(editorInstance);
         },
       });
 
-      setEditor(editorInstance);
+      editorInstanceRef.current = editorInstance;
     }
 
     return () => {
-      if (editor?.destroy) {
-        editor.destroy();
+      if (editorInstanceRef.current?.destroy) {
+        editorInstanceRef.current.destroy();
         setEditor(null);
+        editorInstanceRef.current = null;
       }
     };
-  }, [editor, setEditor]);
+  }, []); // 마운트/언마운트 시에만 실행
 
   return <div id="editorjs" />;
-}
+});
+
+export default EditorContent;
